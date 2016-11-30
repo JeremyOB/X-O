e500c231209984fc22ac98fc6ab57ec02974f9ca
skintly <skintly@gmail.com>
2016-11-29 20:32:34 -0500

    condenced makeX function to work on all boxes insted of a function for each box

--- logic.js
+++ logic.js
@@ -1,98 +1,72 @@
-// alert('Get ready to lose!')
-// i=0
-// while (i = 0 ) {
-//     text += "The number is " + i;
-//   if wincondition   i++;
+console.log("js linked")
+
+
+function makeX(){
+
+  var target = event.target || event.srcElement;
+  var id = target.id
+
+  document.getElementById(id).className = "box-style X";
+  console.log(target)
+}
+
+
+
+// document.getElementById("s-1").onclick = function() {myFunction1()};
+
+// function myFunction1() {
+//     document.getElementById("s-1").className = "box-style X";
+//     console.log("myFunction1")
+// }
+
+// document.getElementById("s-2").onclick = function() {myFunction2()};
+
+// function myFunction2() {
+//     document.getElementById("s-2").className = "box-style X";
+//     alert('Jeremy wins!')
 // }
 
 
-// function MakeX(div){
-//      var state = document.getElementById("s-0");
-//      state.className = (state.className == "Box style X") //? "first_name" : "second_name";
-//  }
+// document.getElementById("s-3").onclick = function() {myFunction3()};
 
-//  function MakeO(div) {
-//   var className = div.getAttribute("class");
-//   if(className=="box-style empty_1") {
-//     div.className = "box-style X";
-//   }
-//   else{
-//     div.className = "box-style O";
-//   }
-// }
-
-// object.addEventListener("click", MakeX);
-
-
-
-// onclick = function() {myFunction0()};
-
-// function myFunction0() {
-//     document.getElementById().className = "box-style X";
+// function myFunction3() {
+//     document.getElementById("s-3").className = "box-style X";
 // }
 
 
-document.getElementById('div').onclick = function() {myFunction0()};
+// document.getElementById("s-4").onclick = function() {myFunction4()};
 
-function myFunction0() {
-    document.getElementById("div").className = "box-style X";
-}
-
-
-document.getElementById("s-1").onclick = function() {myFunction1()};
-
-function myFunction1() {
-    document.getElementById("s-1").className = "box-style X";
-}
-
-document.getElementById("s-2").onclick = function() {myFunction2()};
-
-function myFunction2() {
-    document.getElementById("s-2").className = "box-style X";
-    alert('Jeremy wins!')
-}
+// function myFunction4() {
+//     document.getElementById("s-4").className = "box-style X";
+// }
 
 
-document.getElementById("s-3").onclick = function() {myFunction3()};
+// document.getElementById("s-5").onclick = function() {myFunction5()};
 
-function myFunction3() {
-    document.getElementById("s-3").className = "box-style X";
-}
-
-
-document.getElementById("s-4").onclick = function() {myFunction4()};
-
-function myFunction4() {
-    document.getElementById("s-4").className = "box-style X";
-}
+// function myFunction5() {
+//     document.getElementById("s-5").className = "box-style X";
+// }
 
 
-document.getElementById("s-5").onclick = function() {myFunction5()};
+// document.getElementById("s-6").onclick = function() {myFunction6()};
 
-function myFunction5() {
-    document.getElementById("s-5").className = "box-style X";
-}
-
-
-document.getElementById("s-6").onclick = function() {myFunction6()};
-
-function myFunction6() {
-    document.getElementById("s-6").className = "box-style X";
-}
+// function myFunction6() {
+//     document.getElementById("s-6").className = "box-style X";
+// }
 
 
-document.getElementById("s-7").onclick = function() {myFunction7()};
+// document.getElementById("s-7").onclick = function() {myFunction7()};
 
-function myFunction7() {
-    document.getElementById("s-7").className = "box-style X";
-}
+// function myFunction7() {
+//     document.getElementById("s-7").className = "box-style X";
+// }
 
 
-document.getElementById("s-8").onclick = function() {myFunction8()};
+// document.getElementById("s-8").onclick = function() {myFunction8()};
 
-function myFunction8() {
-    document.getElementById("s-8").className = "box-style X";
-}
+// function myFunction8() {
+//     document.getElementById("s-8").className = "box-style X";
+// }
 
 
 
