4a61be3079bcd6a78d7be28ceb19cdb339ae4955
skintly <skintly@gmail.com>
2016-11-29 21:03:43 -0500

    moving onclick into logic.js with event listener

--- logic.js
+++ logic.js
@@ -1,6 +1,10 @@
 console.log("js linked")
 
-
+window.onload = function()
+    {
+        document.getElementById("MyElement").addEventListener( 'click', changeClass);
+    }
+    
 function makeX(){
 
   var target = event.target || event.srcElement;
@@ -9,70 +13,3 @@ function makeX(){
   document.getElementById(id).className = "box-style X";
   console.log(target)
 }
-
-
-
-// document.getElementById("s-1").onclick = function() {myFunction1()};
-
-// function myFunction1() {
-//     document.getElementById("s-1").className = "box-style X";
-//     console.log("myFunction1")
-// }
-
-// document.getElementById("s-2").onclick = function() {myFunction2()};
-
-// function myFunction2() {
-//     document.getElementById("s-2").className = "box-style X";
-//     alert('Jeremy wins!')
-// }
-
-
-// document.getElementById("s-3").onclick = function() {myFunction3()};
-
-// function myFunction3() {
-//     document.getElementById("s-3").className = "box-style X";
-// }
-
-
-// document.getElementById("s-4").onclick = function() {myFunction4()};
-
-// function myFunction4() {
-//     document.getElementById("s-4").className = "box-style X";
-// }
-
-
-// document.getElementById("s-5").onclick = function() {myFunction5()};
-
-// function myFunction5() {
-//     document.getElementById("s-5").className = "box-style X";
-// }
-
-
-// document.getElementById("s-6").onclick = function() {myFunction6()};
-
-// function myFunction6() {
-//     document.getElementById("s-6").className = "box-style X";
-// }
-
-
-// document.getElementById("s-7").onclick = function() {myFunction7()};
-
-// function myFunction7() {
-//     document.getElementById("s-7").className = "box-style X";
-// }
-
-
-// document.getElementById("s-8").onclick = function() {myFunction8()};
-
-// function myFunction8() {
-//     document.getElementById("s-8").className = "box-style X";
-// }
-
-
-
-
-
-
-
-
-
