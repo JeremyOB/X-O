00c513c12fbc940a967d7298c09a5d67b4c992d4
skintly <skintly@gmail.com>
2016-12-05 21:47:58 -0500

    lisner working in logic file. added switch to O condition

--- logic.js
+++ logic.js
@@ -1,19 +1,28 @@
-console.log("js linked")
-
-
-window.onload = function()
-    {
-      
-        var target = event.target || event.srcElement; //get the info of the object that has been clicked
-        var id = target.id  //make variable "id" equal the id of the clicked object
-        document.getElementById(id).addEventListener( 'click', makeX); // listen for a click and exicute the function makeX
-    }
-    
 function makeX(){
 
-  document.getElementById(id).className = "box-style X";
-  console.log(target)
-}
+  var target = event.target || event.srcElement;//get the info of the object that has been clicked
+  var clicked_id = target.id //make variable "clicked_id" equal the id of the clicked object
 
-//Dan rocks
-// where can i find these rocks? 
+
+  if  (document.getElementById(clicked_id).className.match(/(?:^|\s)box-style X(?!\S)/) ){//check to see if the clicked_id is X 
+       document.getElementById(clicked_id).className = "box-style O";} //if clicke_id is X make it an O
+  else{ 
+       document.getElementById(clicked_id).className = "box-style X";//if clicked_id is not an X make it X
+       console.log(target)
+};
+ 
+}
+ window.onload = function() // need to some how put into one line...
+    {
+      document.getElementById("s-0").addEventListener( 'click', makeX);//If "s-0" is clicke on call function make X
+      document.getElementById("s-1").addEventListener( 'click', makeX);
+      document.getElementById("s-2").addEventListener( 'click', makeX);
+      document.getElementById("s-3").addEventListener( 'click', makeX);
+      document.getElementById("s-4").addEventListener( 'click', makeX);
+      document.getElementById("s-5").addEventListener( 'click', makeX);
+      document.getElementById("s-6").addEventListener( 'click', makeX);
+      document.getElementById("s-7").addEventListener( 'click', makeX);
+      document.getElementById("s-8").addEventListener( 'click', makeX);
+    }
+
+    console.log("js linked") // show if the page has loaded the .js file
