e6bed2701dbb1a7cb056db81dce38c0f2a554903
skintly <skintly@gmail.com>
2016-12-12 07:42:35 -0500

    added win cases as well as a run game funtion. also changed function get state to put classes into string from the array so that we can see if the game state is equal a win case

--- logic.js
+++ logic.js
@@ -1,37 +1,83 @@
-function makeX(){
+getState();
+
+window.onload = function(){ //adds event listner to all the div ids given
+  var elements = document.querySelectorAll("#s-0, #s-1, #s-2, #s-3, #s-4, #s-5, #s-6, #s-7, #s-8"); // all the dive ids give
+  for (var i = 0; i < elements.length; i++) { 
+    elements[i].addEventListener('click', runGame);}
+}
+
+
+function runGame(){
+  changeChar();
+  getState();
+  checkGameState();
+}
+
+var gameState = "new game"
+
+var winConbo1 = "box-container,X box,X box,X box,empty box,empty box,empty box,empty box,empty box,empty box"  
+var winConbo2 = "box-container,empty box,empty box,empty box,X box,X box,X box,empty box,empty box,empty box"  
+var winConbo3 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,X box,X box,X box"  
+var winConbo4 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
+var winConbo5 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
+var winConbo6 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
+var winConbo7 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
+var winConbo8 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box" 
+
+
+function changeChar(){ //change div class to x or o
 
   var target = event.target || event.srcElement;//get the info of the object that has been clicked
   var clicked_id = target.id //make variable "clicked_id" equal the id of the clicked object
 
-
-  if  (document.getElementById(clicked_id).className.match(/(?:^|\s)box-style X(?!\S)/) ){//check to see if the clicked_id is X 
-       document.getElementById(clicked_id).className = "box-style O";} //if clicke_id is X make it an O
+  if  (document.getElementById(clicked_id).className.match(/(?:^|\s)X box(?!\S)/) ){//check to see if the clicked_id is X 
+       document.getElementById(clicked_id).className = "O box";} //if clicke_id is X make it an O
   else{ 
-       document.getElementById(clicked_id).className = "box-style X";//if clicked_id is not an X make it X
+       document.getElementById(clicked_id).className = "X box";//if clicked_id is not an X make it X
        console.log(target)
-};
- 
+     };
 }
- window.onload = function() // need to some how put into one line...
-    {
-      document.getElementById("s-0").addEventListener( 'click', makeX);//If "s-0" is clicke on call function make X
-      document.getElementById("s-1").addEventListener( 'click', makeX);
-      document.getElementById("s-2").addEventListener( 'click', makeX);
-      document.getElementById("s-3").addEventListener( 'click', makeX);
-      document.getElementById("s-4").addEventListener( 'click', makeX);
-      document.getElementById("s-5").addEventListener( 'click', makeX);
-      document.getElementById("s-6").addEventListener( 'click', makeX);
-      document.getElementById("s-7").addEventListener( 'click', makeX);
-      document.getElementById("s-8").addEventListener( 'click', makeX);
-    }
-
-    console.log("js linked") // show if the page has loaded the .js file
 
 //working on enumeration here..,
+function getState(){
+
  var allElements = document.getElementsByTagName("*");
- var allIds = [];
-for (var i = 0, n = allElements.length; i < n; ++i) {
- var el = allElements[i];
- if (el.id) { allIds.push(el.id); }
+ var allClass = [];
+ for (var i = 0, n = allElements.length; i < n; ++i) {
+   var el = allElements[i];
+     if (el.className) { allClass.push(el.className); 
+     }
+ }
+
+  gameState = String(allClass)
+  console.log(gameState);
+ 
 }
-window.alert(allIds);
+
+
+function checkGameState(){
+  if (gameState == winConbo1 || winConbo2 || winConbo3 ) {
+    alert("Game Over")
+};
+}
+
+
+
+
+
+
+      // document.getElementById("s-0").addEventListener( 'click', makeX);
+      // document.getElementById("s-1").addEventListener( 'click', makeX);
+      // document.getElementById("s-2").addEventListener( 'click', makeX);
+      // document.getElementById("s-3").addEventListener( 'click', makeX);
+      // document.getElementById("s-4").addEventListener( 'click', makeX);
+      // document.getElementById("s-5").addEventListener( 'click', makeX);
+      // document.getElementById("s-6").addEventListener( 'click', makeX);
+      // document.getElementById("s-7").addEventListener( 'click', makeX);
+      // document.getElementById("s-8").addEventListener( 'click', makeX);
+    
+
+    console.log("js linked"); // show if the page has loaded the .js file
+
+
+
