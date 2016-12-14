6b78f5620e0218fcddd6b44f10661fa6c18f48fe
skintly <skintly@gmail.com>
2016-12-13 23:40:46 -0500

    added turn based marking of the boxes. added index for logic.js. added all winconbos/states for x. need to work on getting all win stated and add a tie case.

--- logic.js
+++ logic.js
@@ -3,41 +3,49 @@ getState();
 window.onload = function(){ //adds event listner to all the div ids given
   var elements = document.querySelectorAll("#s-0, #s-1, #s-2, #s-3, #s-4, #s-5, #s-6, #s-7, #s-8"); // all the dive ids give
   for (var i = 0; i < elements.length; i++) { 
-    elements[i].addEventListener('click', runGame);}
+    elements[i].addEventListener('click', runGame);
+  }
 }
 
-
+ var turn = 1;
 function runGame(){
-  changeChar();
+ 
+  if (turn == 1) {markX(); turn = 2}
+  else{markO(); turn = 1};
   getState();
   checkGameState();
+
+
 }
 
-var gameState = "new game"
+var allwinningcombos = [
+ "box-container,X box,X box,X box,empty box,empty box,empty box,empty box,empty box,empty box",  //horizontal top
+ "box-container,empty box,empty box,empty box,X box,X box,X box,empty box,empty box,empty box",  //horizontal middle 
+ "box-container,empty box,empty box,empty box,empty box,empty box,empty box,X box,X box,X box",  //horizontal bottom
+ "box-container,x box,empty box,empty box,X box,empty box,empty box,X box,empty box,empty box",  //vertical left
+ "box-container,empty box,X box,empty box,empty box,X box,empty box,empty box,X box,empty box",  //vertical middle
+ "box-container,empty box,empty box,X box,empty box,empty box,X box,empty box,empty box,X box",  //vertical right
+ "box-container,X box,empty box,empty box,empty box,X box,empty box,empty box,empty box,X box",  //diaganal \
+ "box-container,empty box,empty box,X box,empty box,X box,empty box,X box,empty box,empty box",  //diaganal /
+  ];
 
-var winConbo1 = "box-container,X box,X box,X box,empty box,empty box,empty box,empty box,empty box,empty box"  
-var winConbo2 = "box-container,empty box,empty box,empty box,X box,X box,X box,empty box,empty box,empty box"  
-var winConbo3 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,X box,X box,X box"  
-var winConbo4 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
-var winConbo5 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
-var winConbo6 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
-var winConbo7 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box"  
-var winConbo8 = "box-container,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box,empty box" 
+  
 
-
-function changeChar(){ //change div class to x or o
+function markX(){ //change div class to x or o
 
   var target = event.target || event.srcElement;//get the info of the object that has been clicked
   var clicked_id = target.id //make variable "clicked_id" equal the id of the clicked object
+  document.getElementById(clicked_id).className = "X box";
 
-  if  (document.getElementById(clicked_id).className.match(/(?:^|\s)X box(?!\S)/) ){//check to see if the clicked_id is X 
-       document.getElementById(clicked_id).className = "O box";} //if clicke_id is X make it an O
-  else{ 
-       document.getElementById(clicked_id).className = "X box";//if clicked_id is not an X make it X
-       console.log(target)
-     };
 }
 
+function markO(){ //change div class to o
+
+  var target = event.target || event.srcElement;//get the info of the object that has been clicked
+  var clicked_id = target.id //make variable "clicked_id" equal the id of the clicked object
+  document.getElementById(clicked_id).className = "O box";
+
+}
 //working on enumeration here..,
 function getState(){
 
@@ -56,28 +64,31 @@ function getState(){
 
 
 function checkGameState(){
-  if (gameState == winConbo1 || winConbo2 || winConbo3 ) {
-    alert("Game Over")
-};
+  
+  for (var i = 0, n = allwinningcombos.length; i < n; ++i) {
+
+    console.log(i)
+      if (gameState == allwinningcombos[i]) {
+    alert("Game Over");
+
+    }
+    
+  }
 }
 
+console.log("js linked"); // show if the page has loaded the .js file
 
 
 
+// function markX(){ //change div class to x or o
 
+//   var target = event.target || event.srcElement;//get the info of the object that has been clicked
+//   var clicked_id = target.id //make variable "clicked_id" equal the id of the clicked object
 
-      // document.getElementById("s-0").addEventListener( 'click', makeX);
-      // document.getElementById("s-1").addEventListener( 'click', makeX);
-      // document.getElementById("s-2").addEventListener( 'click', makeX);
-      // document.getElementById("s-3").addEventListener( 'click', makeX);
-      // document.getElementById("s-4").addEventListener( 'click', makeX);
-      // document.getElementById("s-5").addEventListener( 'click', makeX);
-      // document.getElementById("s-6").addEventListener( 'click', makeX);
-      // document.getElementById("s-7").addEventListener( 'click', makeX);
-      // document.getElementById("s-8").addEventListener( 'click', makeX);
-    
-
-    console.log("js linked"); // show if the page has loaded the .js file
-
-
-
+//   if  (document.getElementById(clicked_id).className.match(/(?:^|\s)X box(?!\S)/) ){//check to see if the clicked_id is X 
+//        document.getElementById(clicked_id).className = "O box";} //if clicke_id is X make it an O
+//   else{ 
+//        document.getElementById(clicked_id).className = "X box";//if clicked_id is not an X make it X
+//        console.log(target)
+//      };
+// }
